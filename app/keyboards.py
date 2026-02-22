dfa5d219fcd70e52d647eff5e9a63742beb8f61a
Ruslan Bakhtiev <rootpravil066@gmail.com>
2026-02-22 10:06:18 +0300

    update keyboards

--- app/keyboards.py
+++ app/keyboards.py
@@ -1,23 +1,18 @@
 from aiogram.types import ReplyKeyboardMarkup, KeyboardButton, InlineKeyboardMarkup, InlineKeyboardButton
 from aiogram.utils.keyboard import ReplyKeyboardBuilder, InlineKeyboardBuilder
 
-main=InlineKeyboardMarkup(inline_keyboard=[
-    [InlineKeyboardButton(text="Создать комнату", callback_data="create_game")],
-    [InlineKeyboardButton(text="Список комнат", callback_data="rooms_list")],
-])
+main=ReplyKeyboardMarkup(keyboard=[
+    [KeyboardButton(text="Создать комнату", callback_data="create_game")],
+    [KeyboardButton(text="Список комнат", callback_data="rooms_list")],
+],
+                        resize_keyboard=True,
+)
 
-
-#settings = InlineKeyboardMarkup(inline_keyboard=[
-#    [InlineKeyboardButton(text='Пук', url='https://kwork.ru/user/sadfrog')]
-#])
-
-
-cars = ['Tesla', 'Mercedes', 'Audi']
 
 async def inline_cars():
     # keyboard = ReplyKeyboardBuilder()
+    data = []
     keyboard = InlineKeyboardBuilder()
-    for car in cars:
-       # keyboard.add(KeyboardButton(text=car))
-       keyboard.add(InlineKeyboardButton(text=car, url="https://kwork.ru/user/sadfrog"))
+    for item in data:
+       keyboard.add(InlineKeyboardButton(text=item.name, url=""))
     return keyboard.adjust(2).as_markup()
